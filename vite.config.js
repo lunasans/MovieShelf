380bee8d306d5eb3f8f776f4cae37bd7adcf97d0
Lunasans <rene@rene-neuhaus.eu>
2026-04-05 20:25:19 +0200

    chore: remove vite-plugin-pwa dependency and configuration

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
-        }),
-        VitePWA({
-            registerType: 'autoUpdate',
-            manifest: false, // We already have a manual manifest in public/manifest.json
         }),
     ],
 });
