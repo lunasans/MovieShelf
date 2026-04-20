6e169e109cf704fe69bb7379e9145b31ebb1a2d6
Lunasans <rene@rene-neuhaus.eu>
2026-04-20 17:31:41 +0200

    Entferne Auth-Middleware aus dem ListController und aktualisiere die Shelf-Version auf 2.17.1

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -9,11 +9,6 @@ use Illuminate\Http\Request;
 
 class ListController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     public function index()
     {
         $lists = MovieList::where('user_id', auth()->id())
